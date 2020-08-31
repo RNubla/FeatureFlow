aa84325a5da9e26f0f9a93b4c9be7386759b6ddb
RNubla <rnubla97@gmail.com>
2020-08-31 18:42:33 -0400

    refactor code for readability

--- split_video_sections.py
+++ split_video_sections.py
@@ -22,24 +22,31 @@ class CheckResolution:
         height = self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
         return int(height)
 
-class Resolution360P:
+class VideoDecimation:
+    def __init__(self, input_path_from_gui : str, output_path_from_gui : str):
+        self. current_working_dir : str = Path.cwd()
+        self.input_video : str = input_path_from_gui
+        self.output_path : str = output_path_from_gui
+
+    def removeDuplicateFrames(self):
+        filename = self.dir_name_based_off_filename
+        file = self.path_to_dir_name_based_off_filename / filename
+        print(file)
+        print('ffmpeg.exe  -qscale 0 -i ' + str(self.file_name) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
+        os.system('ffmpeg.exe -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
+        os.system(str(ffmpeg_exe) + ' -i '+ str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
+
+class Resolution360p:
     def __init__(self, file_input_path_from_gui : str, interpolation : int, output_path_from_gui : str):
         self.current_working_dir : str = Path.cwd()
         self.input_video : str = file_input_path_from_gui
-        self.file_name : str = os.path.splitext(file_input_path_from_gui)[0]   # returns file name from example.mp4 to example
+        self.video_name : str = os.path.splitext(os.path.basename(file_input_path_from_gui))[0]   # returns file name from example.mp4 to example
         self.interp_num : int = interpolation 
         self.output_path : str = output_path_from_gui
 
-        self.finished_file_name : str = Path.cwd() / str(self.file_name + '-final.mp4')
+        self.finished_file_name : str = Path.cwd() / str(self.video_name + '-final.mp4')
         self.interp_output_file_name : str = Path.cwd() / 'output.mp4'
 
-    # def removeDupFrames(self):
-        # filename = self.dir_name_based_off_filename
-        # file = self.path_to_dir_name_based_off_filename / filename
-        # print(file)
-        # print('ffmpeg.exe  -qscale 0 -i ' + str(self.file_name) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
-        # os.system('ffmpeg.exe -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
-        # os.system(str(ffmpeg_exe) + ' -i '+ str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
 
     def runFeatureFlow(self):
         print(self.interp_num)
@@ -49,7 +56,7 @@ class Resolution360P:
 
         print(Path(self.interp_output_file_name) / Path(self.output_path))
         time.sleep(3)
-        shutil.move('output.mp4', Path(self.interp_output_file_name) / Path(self.output_path))
+        shutil.move('output.mp4', str(Path(self.output_path) / '{}-final.mp4'.format(self.video_name)))
 
     def deleteFiles(self):
         cwd = Path(self.current_working_dir)
@@ -59,183 +66,57 @@ class Resolution360P:
         print(cwd_decimated_file)
         os.remove(cwd_decimated_file)
 
-class Resolution720P:
+class Resolution720p:
+    def __init__(self, file_input_path_from_gui : str, interpolation : int, output_path_from_gui : str):
+        self.current_working_dir : str = Path.cwd()
+        self.input_video : str = file_input_path_from_gui
+        self.video_name : str = os.path.splitext(os.path.basename(self.input_video))[0]   # returns file name from example.mp4 to example
+        self.interp_num : int = interpolation 
+        self.output_path : str = output_path_from_gui
 
-    def __init__(self, file_name, interpolation, output_path_from_gui):
-        self.file_name = file_name
-        self.interpolation = int(interpolation)
-        self.input_dir = ''
-        self.top_left_dir = ''
-        self.top_right_dir = ''
-        self.bottom_left_dir = ''
-        self.bottom_right_dir = ''
-        self.file_dir_name = os.path.splitext(self.file_name)[0]
-        self.output_dir = ''
-        self.output_file_dir = ''
-        self.current_working_dir = ''
-        self.filename_dir = ''
-        # self.model_dir = './models/bdcn/final-model/bdcn_pretrained_on_bsds500.pth'
-        # self.model_dir = '.\\models\\bdcn\\final-model\\bdcn_pretrained_on_bsds500.pth'
-        self.output_path = output_path_from_gui
+        self.finished_file_name : str = Path.cwd() / str(self.video_name + '-final.mp4')
+        self.interp_output_file_name : str = Path.cwd() / 'output.mp4'
 
-        if self.interpolation % 2 != 0:
-            # print('Use the power of two for interpolation')
-            sys.exit('Error!: Use the power of two for interpolation')
+    def splitVideoIntoSections(self):
+        print('Video Name: ',self.video_name)
+        input_split_dict = {
+            'top-left' : [self.input_video, "crop=iw/2:ih/2:0:0", str(Path(self.output_path) / '{}-top-left.mp4').format(self.video_name)],
+            'top-right' : [self.input_video, "crop=iw/2:ih/2:ow:0", str(Path(self.output_path) / '{}-top-right.mp4').format(self.video_name)],
+            'bottom-left' : [self.input_video, "crop=iw/2:ih/2:0:oh", str(Path(self.output_path) / '{}-bottom-left.mp4').format(self.video_name)],
+            'bottom-right' : [self.input_video, "crop=iw/2:ih/2:ow:oh", str(Path(self.output_path) / '{}-bottom-right.mp4').format(self.video_name)],
+        }
 
+        for v in input_split_dict.items():
+            # print(v[1][0])
+            # print(v[1][1])
+            print(str(ffmpeg_exe) + ' -i ' + v[1][0] + ' -vsync 0 -frame_pts true -vf '+'"' + v[1][1] +'"' +' '+ v[1][2])
+            os.system(str(ffmpeg_exe) + ' -i ' + v[1][0] + ' -vsync 0 -frame_pts true -vf '+'"' + v[1][1] +'"' +' '+ v[1][2])
 
-    def create_dir(self):
-                    
-        self.current_working_dir = os.getcwd()
-        self.input_dir = Path(self.current_working_dir + '/input/')
-        self.filename_dir = self.input_dir / self.file_dir_name
-        self.top_left_dir = self.filename_dir / 'top-left'
-        self.top_right_dir = self.filename_dir / 'top-right'
-        self.bottom_left_dir = self.filename_dir / 'bottom-left'
-        self.bottom_right_dir = self.filename_dir / 'bottom-right'
-
-        # self.output_dir = Path(self.current_working_dir + '/output/')
-        self.output_dir = Path(self.output_path + '/output/')
-        self.output_file_dir = self.output_dir / self.file_dir_name
-        
-        Path(self.output_file_dir).mkdir(parents=True, exist_ok=True)
-        Path(self.filename_dir).mkdir(parents=True, exist_ok=True)
-        Path(self.top_left_dir).mkdir(parents=True, exist_ok=True)
-        Path(self.top_right_dir).mkdir(parents=True, exist_ok=True)
-        Path(self.bottom_left_dir).mkdir(parents=True, exist_ok=True)
-        Path(self.bottom_right_dir).mkdir(parents=True, exist_ok=True)
-
-        print(self.file_dir_name)
-
-    def remove_dup_frames(self):
-        filename = self.file_dir_name
-        file = self.input_dir / filename
-        print(file)
-        print('ffmpeg -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
-        # os.system('ffmpeg -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
-        os.system(str(ffmpeg_exe) + ' -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
     
-    def run_splitter(self):
-        filename = self.file_dir_name
-        file = str(self.input_dir / filename)    # /cwd/input/filename
+    def runFeatureFlow(self):
+        input_split_files = [str(Path(self.output_path) / '{}-top-left.mp4'.format(self.video_name)), 
+                             str(Path(self.output_path) / '{}-top-right.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) / '{}-bottom-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) / '{}-bottom-right.mp4'.format(self.video_name))
+                             ]
 
-        top_left_dir = str(self.top_left_dir)
-        os.system(str(ffmpeg_exe) + ' -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:0:0" ' + top_left_dir + '/' +'decimated-top-left.mp4')
-        print('ffmpeg -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:0:0" ' + top_left_dir + '/' +'decimated-top-left.mp4')
-
-        # remove top right
-        top_right_dir = str(self.top_right_dir)
-        os.system(str(ffmpeg_exe) + ' -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:ow:0" ' + top_right_dir + '/' +'decimated-top-right.mp4')
-        print('ffmpeg -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:ow:0" ' + top_right_dir + '/' +'decimated-top-right.mp4')
-        
-
-        # remove bottom left
-        bottom_left_dir = str(self.bottom_left_dir)
-        os.system(str(ffmpeg_exe) + ' -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:0:oh" ' + bottom_left_dir + '/' +'decimated-bottom-left.mp4')
-        print('ffmpeg -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:0:oh" ' + bottom_left_dir + '/' +'decimated-bottom-left.mp4')
-        
-
-        # remove bottom right
-        bottom_right_dir = str(self.bottom_right_dir)
-        os.system(str(ffmpeg_exe) + ' -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:ow:oh" ' + bottom_right_dir + '/' +'decimated-bottom-right.mp4')
-        print('ffmpeg -i ' + file + '-decimated.mp4 -vsync 0 -frame_pts true -vf "crop=iw/2:ih/2:ow:oh" ' + bottom_right_dir + '/' +'decimated-bottom-right.mp4')
-        
+        for video in input_split_files:
+            print(video)
+            main(self.interp_num, video)
+            shutil.move('output.mp4', video)
     
-    def run_feature_flow(self):
-        interpolation_num = self.interpolation
-        filename = self.file_dir_name
-        cwd = Path(self.current_working_dir)
-        cwd_output_file = cwd / 'output.mp4'
-        # TOP LEFT
-        print('INTERP TOP LEFT')
-        top_left_file = str(self.top_left_dir / 'decimated-top-left.mp4')
-        print('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_left_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_left_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('set CUDA_VISIBLE_DEVICES= 0 & python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_left_file + ' --t_interp ' + str(interpolation_num))
-        # main(interpolation_num, str(self.model_dir) ,'checkpoints/FeFlow.ckpt', top_left_file)
-        # print(interpolation_num)
-        main(interpolation_num, top_left_file)
-
-        print("!!!CWD OUTPUT")
-        print(cwd_output_file)
-        print(str(self.output_file_dir))
-        shutil.move(str(cwd_output_file), str(self.output_file_dir))
-        time.sleep(5)
-        
-        print('RENAME')
-        print(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'top-left.mp4')
-        os.rename(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'top-left.mp4')
-        
-        # TOP RIGHT
-        print('INTERP TOP RIGHT')
-        top_right_file = str(self.top_right_dir / 'decimated-top-right.mp4')
-        print('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_right_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_right_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('set CUDA_VISIBLE_DEVICES= 0 & python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + top_right_file + ' --t_interp ' + str(interpolation_num))
-        # main(interpolation_num, str(self.model_dir) ,'checkpoints/FeFlow.ckpt', top_right_file)
-        main(interpolation_num, top_right_file)
-        # MOVE
-        shutil.move(str(cwd_output_file), str(self.output_file_dir))
-        time.sleep(5)
-        
-        print('RENAME')
-        print(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'top-right.mp4')
-        os.rename(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'top-right.mp4')
-        
-        # BOTTOM LEFT
-        print('INTERP BOTTOM LEFT')
-        bottom_left_file = str(self.bottom_left_dir / 'decimated-bottom-left.mp4')
-        print('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_left_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_left_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('set CUDA_VISIBLE_DEVICES= 0 & python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_left_file + ' --t_interp ' + str(interpolation_num))
-        # main(interpolation_num, str(self.model_dir)  ,'checkpoints/FeFlow.ckpt', bottom_left_file)
-        main(interpolation_num, bottom_left_file)
-
-        shutil.move(str(cwd_output_file), str(self.output_file_dir))
-        time.sleep(5)
-        
-        print('RENAME')
-        print(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'bottom-left.mp4')
-        os.rename(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'bottom-left.mp4')
-        
-        # BOTTOM RIGHT
-        print('INTERP BOTTOM RIGHT')
-        bottom_right_file = str(self.bottom_right_dir / 'decimated-bottom-right.mp4')
-        print('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_right_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('CUDA_VISIBLE_DEVICES=0 python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_right_file + ' --t_interp ' + str(interpolation_num))
-        # os.system('set CUDA_VISIBLE_DEVICES= 0 & python sequence_run.py --checkpoint checkpoints/FeFlow.ckpt --video_path ' + bottom_right_file + ' --t_interp ' + str(interpolation_num))
-        # main(interpolation_num, str(self.model_dir)  ,'checkpoints/FeFlow.ckpt', bottom_right_file)
-        main(interpolation_num, bottom_right_file)
-        shutil.move(str(cwd_output_file), str(self.output_file_dir))
-        
-        time.sleep(5)
-        print('RENAME')
-        print(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'bottom-right.mp4')
-        os.rename(str(self.output_file_dir / 'output.mp4'), cwd / 'output' / self.file_dir_name /  'bottom-right.mp4')
-
-
-
-
-    def stitch_sections(self):
-        filename = self.file_dir_name
-        os.system(str(ffmpeg_exe) + ' -i ' + str(self.output_file_dir / 'top-left.mp4') +
-                        ' -i ' + str(self.output_file_dir / 'top-right.mp4') + 
-                        ' -i ' + str(self.output_file_dir / 'bottom-left.mp4') + 
-                        ' -i ' + str(self.output_file_dir / 'bottom-right.mp4') + 
+    def stitchVideo(self):
+        input_split_files = [str(Path(self.output_path) / '{}-top-left.mp4'.format(self.video_name)), 
+                             str(Path(self.output_path) / '{}-top-right.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) / '{}-bottom-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) / '{}-bottom-right.mp4'.format(self.video_name))
+                             ]
+        os.system(str(ffmpeg_exe) + 
+                        ' -i ' + input_split_files[0] +
+                        ' -i ' + input_split_files[1] + 
+                        ' -i ' + input_split_files[2] + 
+                        ' -i ' + input_split_files[3] + 
                         ' -filter_complex "[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2[v]" -map "[v]" ' + 
-                        str(self.output_dir / filename) + '-final.mp4')
-        print((str(ffmpeg_exe) + ' -i ' + str(self.output_file_dir / 'top-left.mp4') +
-                        ' -i ' + str(self.output_file_dir / 'top-right.mp4') + 
-                        ' -i ' + str(self.output_file_dir / 'bottom-left.mp4') + 
-                        ' -i ' + str(self.output_file_dir / 'bottom-right.mp4') + 
-                        ' -filter_complex "[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2[v]" -map "[v]" ' + 
-                        str(self.output_dir / filename) + '-final.mp4'))
-
-    def delete_files(self):
-        filename = self.file_dir_name
-        decimated_file = str(Path(self.input_dir / filename)) + '-decimated.mp4'
-        print(self.filename_dir)
-        print(decimated_file)
-        print(self.output_file_dir)
-        shutil.rmtree(self.filename_dir)
-        os.remove(decimated_file)
-        shutil.rmtree(self.output_file_dir)
+                        str(Path(self.output_path) / '{}-final.mp4'.format(self.video_name)))
+        for video in input_split_files:
+            os.remove(video)
