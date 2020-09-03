cadd40547f4db900615c8cfa12316ff4d2fc89d3
RNubla <rnubla97@gmail.com>
2020-09-03 16:22:54 -0400

    reverted back

--- feature_flow_interface.py
+++ feature_flow_interface.py
@@ -4,8 +4,7 @@ import os , shutil, time
 from pathlib import Path
 
 # from numpy.core.arrayprint import str_format
-# from sequence_run import main, getInterpolationIndex, getInterpolationRange, getIteration
-from .sequence_run import main, getInterpolationIndex, getInterpolationRange, getIteration
+from sequence_run import main, getInterpolationIndex, getInterpolationRange, getIteration
 import cv2
 
 ffmpeg_exe = Path().cwd() / 'ffmpeg.exe'
